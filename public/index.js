a234cfbb59808ef467d16c80a4598ccf46965a70
mike-progam-j <mikeprogamj@gmail.com>
2026-09-06 18:11:31 -0300

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,29 @@
+const express= require('express');
+const app=express();
+const path=require('path');
+
+const users=[]
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+
+app.get('/users',(request, response)=>{
+    
+    
+    
+    response.json({users});
+}
+)  
+
+app.post('/users',(request, response)=>{
+    request.body;
+    const {name, age}=request.body;
+    users.push({name, age});
+    response.json({ok:true});
+}
+)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
